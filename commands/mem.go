6162291d845be17614aff3f73622800251d19ca8
Augusto Dwenger <hamburghammer@gmail.com>
2020-06-29 14:02:06 +0200

    Refactor Mem to use DI to be more testable

--- commands/mem.go
+++ commands/mem.go
@@ -10,12 +10,12 @@ import (
 
 // Mem holds the memory usage for the json transformation
 type Mem struct {
-	Mem string
+	ReadVirtualMemoryStat func() (*mem.VirtualMemoryStat, error)
 }
 
 // NewMem is a constructor for the Mem struct
 func NewMem() Mem {
-	return Mem{}
+	return Mem{mem.VirtualMemory}
 }
 
 // Exec gets the mem value and maps it to the executiondata struct
@@ -24,13 +24,13 @@ func (m Mem) Exec(args args.Arguments) ([]byte, error) {
 		return []byte{}, nil
 	}
 
-	mem, err := mem.VirtualMemory()
+	mem, err := m.ReadVirtualMemoryStat()
 	if err != nil {
 		return []byte{}, err
 	}
 
 	usage := fmt.Sprintf("%d/%d", bytesToMegaByte(mem.Used), bytesToMegaByte(mem.Total))
-	data := Mem{usage}
+	data := struct{ Mem string }{usage}
 	return json.Marshal(data)
 }
 
