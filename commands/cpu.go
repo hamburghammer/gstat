f41fca5f5507b55f7fa8a57d213daf97929e392f
Augusto Dwenger <hamburghammer@gmail.com>
2020-06-29 13:27:21 +0200

    Refactor CPU to use DI to be more testable

--- commands/cpu.go
+++ commands/cpu.go
@@ -2,11 +2,10 @@ package commands
 
 import (
 	"encoding/json"
-	"fmt"
 	"time"
 
 	"github.com/hamburghammer/gstat/args"
-	e "github.com/hamburghammer/gstat/errors"
+	"github.com/hamburghammer/gstat/errors"
 	"github.com/shirou/gopsutil/cpu"
 )
 
@@ -16,11 +15,12 @@ const OperationKeyCPUReading = "CPUReading"
 // CPU holds the config to get the cpu load in percentage
 type CPU struct {
 	TimeInMilSec int
+	ReadCPUStat  func(interval time.Duration, percpu bool) ([]float64, error)
 }
 
 // NewCPU creates a new cpu percentage struct
 func NewCPU() CPU {
-	return CPU{500}
+	return CPU{TimeInMilSec: 500, ReadCPUStat: cpu.Percent}
 }
 
 // Exec gets the cpu value and maps it to the executiondata struct
@@ -28,27 +28,10 @@ func (c CPU) Exec(args args.Arguments) ([]byte, error) {
 	if !args.CPU {
 		return []byte{}, nil
 	}
-	total, err := c.TotalCPU()
+	total, err := c.ReadCPUStat(time.Millisecond*time.Duration(c.TimeInMilSec), false)
 	if err != nil {
-		return []byte{}, err
+		return []byte{}, errors.BaseError{OperationKeyCPUReading, err.Error()}
 	}
-	data := struct{ CPU float64 }{CPU: total}
+	data := struct{ CPU float64 }{CPU: total[0]}
 	return json.Marshal(data)
-}
-
-// TotalCPU returns the first entry of the return array form the given function
-func (c CPU) TotalCPU() (float64, error) {
-	total, err := cpu.Percent(time.Millisecond*time.Duration(c.TimeInMilSec), false)
-
-	if err != nil {
-		wrappedError := fmt.Errorf("Something went wrong reading the CPU: %w", err)
-		return float64(0), e.BaseError{Operation: OperationKeyCPUReading, Message: wrappedError.Error()}
-	}
-
-	if len(total) != 1 {
-		return float64(0), e.BaseError{
-			Operation: OperationKeyCPUReading,
-			Message:   "No CPU data was found. Please check the HOST_PROC env to point to the right directory."}
-	}
-	return total[0], nil
 }
