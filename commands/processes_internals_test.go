e1a70507ec817dd3cce16dc789ad71342cb109b7
Augusto Dwenger <hamburghammer@gmail.com>
2020-08-05 16:22:44 +0200

    Add test for command.Process testing the internals and externals

--- commands/processes_internals_test.go
+++ commands/processes_internals_test.go
@@ -1,6 +1,7 @@
 package commands
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -32,16 +33,105 @@ func TestByCPULen(t *testing.T) {
 	})
 }
 
-// func TestByCPUSwap(t *testing.T) {
-// 	t.Run("epmty array", func(t *testing.T) {
-// 		unswaped := []cpuProcess{{Name: "foo"}, {Name: "bar"}}
-// 		swaped := []cpuProcess{{Name: "bar"}, {Name: "foo"}}
-// 		got := byCPU(unswaped)
-// 		got.Swap(0, 1)
-// 		want := byCPU(swaped)
+func TestByCPUSwap(t *testing.T) {
+	t.Run("swap array items", func(t *testing.T) {
+		unswaped := []cpuProcess{{Name: "foo"}, {Name: "bar"}}
+		swaped := []cpuProcess{{Name: "bar"}, {Name: "foo"}}
 
-// 		if got != want {
-// 			t.Error("does not equal")
-// 		}
-// 	})
-// }
+		got := byCPU(unswaped)
+		got.Swap(0, 1)
+		want := byCPU(swaped)
+
+		assert.Equal(t, want, got)
+	})
+}
+
+func TestByCPULess(t *testing.T) {
+	cpuProcessArray := []cpuProcess{{CPU: 1}, {CPU: 2}}
+
+	t.Run("less on cpu field smaller", func(t *testing.T) {
+
+		got := byCPU(cpuProcessArray).Less(0, 1)
+		want := false
+
+		assert.Equal(t, want, got)
+	})
+
+	t.Run("less on cpu field bigger", func(t *testing.T) {
+
+		got := byCPU(cpuProcessArray).Less(1, 0)
+		want := true
+
+		assert.Equal(t, want, got)
+	})
+}
+
+func TestGetProcessCPUInfos(t *testing.T) {
+	t.Run("transform Process to cpuProcess", func(t *testing.T) {
+		nameFunc := func() (string, error) { return "foo", nil }
+		cpuProcessFunc := func() (float64, error) { return 0, nil }
+		process := Process{Pid: 1, Name: nameFunc, CPUPercent: cpuProcessFunc}
+
+		got, err := getProcessCPUInfos(&process)
+		want := &cpuProcess{Name: "foo", Pid: 1, CPU: 0}
+
+		assert.Nil(t, err, "No error expected")
+		assert.Equal(t, want, got)
+	})
+
+	t.Run("calling name function returns error", func(t *testing.T) {
+		wantErr := errors.New("error")
+
+		nameFunc := func() (string, error) { return "", wantErr }
+		cpuProcessFunc := func() (float64, error) { return 0, nil }
+		process := Process{Pid: 1, Name: nameFunc, CPUPercent: cpuProcessFunc}
+
+		_, gotErr := getProcessCPUInfos(&process)
+
+		assert.NotNil(t, gotErr, "An error was expected")
+		assert.Equal(t, wantErr, gotErr)
+	})
+
+	t.Run("calling cpuProcess function returns error", func(t *testing.T) {
+		wantErr := errors.New("error")
+
+		nameFunc := func() (string, error) { return "", nil }
+		cpuProcessFunc := func() (float64, error) { return 0, wantErr }
+		process := Process{Pid: 1, Name: nameFunc, CPUPercent: cpuProcessFunc}
+
+		_, gotErr := getProcessCPUInfos(&process)
+
+		assert.NotNil(t, gotErr, "An error was expected")
+		assert.Equal(t, wantErr, gotErr)
+	})
+}
+
+func TestGetProcessesCPUInfos(t *testing.T) {
+	err := errors.New("error")
+
+	nameFunc := func() (string, error) { return "foo", nil }
+	nameErrFunc := func() (string, error) { return "foo", err }
+
+	cpuProcessFunc := func() (float64, error) { return 0, nil }
+
+	t.Run("transform array of process into an array of cpuProcess", func(t *testing.T) {
+		processes := []*Process{{Pid: 2, Name: nameFunc, CPUPercent: cpuProcessFunc}, {Pid: 1, Name: nameFunc, CPUPercent: cpuProcessFunc}}
+		got, gotErr := getProcessesCPUInfos(processes)
+		want := []cpuProcess{{Name: "foo", CPU: 0, Pid: 2}, {Name: "foo", CPU: 0, Pid: 1}}
+
+		assert.Nil(t, gotErr, "No error expected")
+		assert.Equal(t, want, got)
+	})
+
+	t.Run("return error directly if one happends", func(t *testing.T) {
+		processes := []*Process{{Pid: 2, Name: nameFunc, CPUPercent: cpuProcessFunc}, {Pid: 1, Name: nameErrFunc, CPUPercent: cpuProcessFunc}}
+		got, gotErr := getProcessesCPUInfos(processes)
+		want := []cpuProcess{{Name: "foo", CPU: 0, Pid: 2}}
+		wantErr := err
+
+		assert.NotNil(t, gotErr, "an error was expected")
+
+		assert.Equal(t, wantErr, gotErr)
+		assert.Equal(t, want, got)
+	})
+}
--- commands/processes_test.go
+++ commands/processes_test.go
@@ -1,1 +1,30 @@
 package commands_test
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/hamburghammer/gstat/args"
+	"github.com/hamburghammer/gstat/commands"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestProcessExec(t *testing.T) {
+	t.Run("should test for the Argument for Process", func(t *testing.T) {
+		arguments := args.Arguments{Processes: false}
+		got, err := commands.Processes{}.Exec(arguments)
+
+		assert.Nil(t, err, "no error expected")
+		assert.Equal(t, 0, len(got))
+	})
+
+	t.Run("should exit if the getting the process data returns an error", func(t *testing.T) {
+		wantErr := errors.New("getting data error")
+		arguments := args.Arguments{Processes: true}
+		mockProcessData := func() ([]*commands.Process, error) { return nil, wantErr }
+		_, err := commands.Processes{ReadProcesses: mockProcessData}.Exec(arguments)
+
+		assert.NotNil(t, err, "an error was expected")
+		assert.Equal(t, wantErr, err)
+	})
+}
